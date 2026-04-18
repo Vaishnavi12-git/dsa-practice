886e5c1760c9a8b24c4f9c8db6c1bae45dd91b09
Vaishnavi Kale <vaishnavikale06032006@gmail.com>
2026-04-18 15:30:31 +0530

    Longest Substring using HashSet

--- Arrays/LongestSubstring.java
+++ Arrays/LongestSubstring.java
@@ -1,23 +1,26 @@
 import java.util.*;
 class LongestSubstring {
     public static int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> map = new HashMap();
+        HashSet<Character> set = new HashSet();
         int left = 0;
         int right = 0;
         int maxLength = 0;
 
-        while(right < s.length()) {
-            char ch = s.charAt(right);
-            if(map.containsKey(ch)){
-                left = Math.max(left, map.get(ch) + 1 );
+        while(right < s.length()){
+            if(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }else {
+                set.add(s.charAt(right));
+                right++;
             }
 
-            map.put(ch, right);
-            maxLength = Math.max(maxLength, right-left+1);
-            right++;
-        } 
+            maxLength = Math.max(maxLength, right - left);
+        }
+
         return maxLength;
     }
+
 
     public static void main(String args[]) {
         String s = "abcabcbb";
